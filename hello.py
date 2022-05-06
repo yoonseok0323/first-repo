3f245ef2117de2a0193d77179b2a611656ea481b
yoonseok0323 <yoonrock02@gamil.com>
2022-05-06 13:32:26 +0900

    효율적

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("hello")
+for i in range(1,21):
+    if i % 15 == 0:
+        print("yeardream", i)
+    elif i % 3 == 0:
+        print("year",i)
+    elif i %5 ==0 :
+        print("dream",i)
+    else:
+        print(i)
